bf6bb553f37bc6ef956822a7cc8f720714efc50e
Raf Mesotten <rafmesotten@gmail.com>
2024-02-05 15:50:26 +0100

    99% final commit - Notebook ready

--- scripts/image_builders.py
+++ scripts/image_builders.py
@@ -7,7 +7,9 @@ Author: Raf Mesotten
 
 # Importing necessary packages
 import numpy as np
-import scripts.image_manipulations as img
+import sys
+sys.path.append('../scripts')  # Adjust the path accordingly
+import image_manipulations as img
 
 
 def tile(image, rows, columns):
@@ -20,52 +22,40 @@ def tile(image, rows, columns):
 
 def grid_with_flips(image, matrix):
     """
-    Create a grid of images with specified flips based on the input matrix. The
-    matrix should be a 2D matrix specifying flip operations for each element.
-    Each element in the matrix can be 1 (horizontal flip), 2 (vertical flip),
-    or 3 (both flips). In all other cases the original image will be returned.
+    Create a grid of images (based on the input image) with specified
+    manipulations (based on the input matrix). Each element in the matrix can
+    be a code (str or int) representing a sequence of manipulations that are
+    available in the 'multiple_manipulations' method. See the docstring of
+    'multiple_manipulations' for details on the code format.
     """
-    # Creating a list with flipped images
-    images_list = []
+    result_images_rows = []
 
     for row in matrix:
-        row_images = []
-        for element in row:
-            if element == 1:
-                row_images.append(img.h_flip(image))
-            elif element == 2:
-                row_images.append(img.v_flip(image))
-            elif element == 3:
-                row_images.append(img.h_flip(img.v_flip(image)))
-            else:
-                row_images.append(image)
-        images_list.append(row_images)
-
-    # Creating 1 big image out of the list with flipped images
-    result_images_rows = []  # Empty list to store rows of images
-
-    for row_images in images_list:
+        row_images = [img.multiple_manipulations(image, element) for element in row]
         row_concatenated_images = np.concatenate(row_images, axis=1)
         result_images_rows.append(row_concatenated_images)
 
     result_image = np.concatenate(result_images_rows, axis=0)
 
-    # Returning the result
     return result_image
-
 
 
 def create_colorful_big_one(image, matrix):
     """
-    Create a colorful composite image based on a given image and a list of
-    color codes.
+    Create a colorful image based on a given image and a list of color codes.
 
     The function takes an image and a matrix of color codes ('r' for red, 'g'
     for green, 'b' for blue) as input. The color codes are mapped clockwise on
     the edges of a square matrix, and the resulting matrix is used to generate
     a composite image with colored segments. In case the input matrix doesn't
     contain a multiple of 4 elements (required to create a square output
-    image), extra (unmodified) images are added.
+    image), extra (unmodified) images are added. The input matrix should
+    contain at least 5 elements. The big image will be in the center, and its
+    size automatically adjusts.
+
+    Examples:
+        matrix_1 = ['r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b']
+        matrix_2 = ['r', 'g', 'b', 'r', 'g', 'b', 'r', 'g', 'b']
     """
     if len(matrix) % 4 != 0:
         # Calculate the number of 'o' elements needed to make len(matrix) % 4 == 0
@@ -76,34 +66,19 @@ def create_colorful_big_one(image, matrix):
     size = 1 + round(len(matrix)/4)
 
     # Define a [size x size] matrix
-    colored_matrix = np.zeros((size, size), dtype='str')
+    manipulation_matrix = np.zeros((size, size), dtype='str')
 
     # Map the colors clockwise on the edges of the matrix
-    colored_matrix[0, :] = matrix[:size]  # Top row
-    colored_matrix[:, -1] = matrix[size-1:2*(size-1)+1]  # Right column
-    colored_matrix[-1, ::-1] = matrix[2*(size-1):3*(size-1)+1]  # Bottom row (reversed)
-    colored_matrix[::-1, 0][:-1] = matrix[3*(size-1):]  # Left column (reversed), except the last element
-
-    # Creating a list with colored images
-    images_list = []
-
-    for row in colored_matrix:
-        row_images = []
-        for element in row:
-            if element == "r":
-                row_images.append(img.color(image, "r"))
-            elif element == "g":
-                row_images.append(img.color(image, "g"))
-            elif element == "b":
-                row_images.append(img.color(image, "b"))
-            else:
-                row_images.append(image)
-        images_list.append(row_images)
+    manipulation_matrix[0, :] = matrix[:size]  # Top row
+    manipulation_matrix[:, -1] = matrix[size - 1:2 * (size - 1) + 1]  # Right column
+    manipulation_matrix[-1, ::-1] = matrix[2 * (size - 1):3 * (size - 1) + 1]  # Bottom row (reversed)
+    manipulation_matrix[::-1, 0][:-1] = matrix[3 * (size - 1):]  # Left column (reversed), except the last element
 
     # Creating 1 big image out of the list with colored images
     result_images_rows = []  # Empty list to store rows of images
 
-    for row_images in images_list:
+    for row in manipulation_matrix:
+        row_images = [img.color(image, code) for code in row]
         row_concatenated_images = np.concatenate(row_images, axis=1)
         result_images_rows.append(row_concatenated_images)
 
--- scripts/image_manipulations.py
+++ scripts/image_manipulations.py
@@ -32,17 +32,17 @@ def color(image, color):
     Color the input image red ('r'), green ('g') or blue ('b'). Return the
     original image in case of an unknown color.
     """
-    if color == 'r':
+    if color == 'r':  # red
         red_image = image.copy()
         red_image[:, :, [1, 2]] = 0
         return red_image
 
-    elif color == 'g':
+    elif color == 'g':  # green
         green_image = image.copy()
         green_image[:, :, [0, 2]] = 0
         return green_image
 
-    elif color == 'b':
+    elif color == 'b':  # blue
         blue_image = image.copy()
         blue_image[:, :, [0, 1]] = 0
         return blue_image
@@ -56,7 +56,7 @@ def color_quantization(image, factor):
     Apply color quantization to an image. The function processes each color
     channel independently and rounds the pixel values to the nearest multiple
     of the specified factor. The shape of the returned array is the same as the
-    input image.
+    shape of the input image.
     """
     quantized_image = image.copy()
     for color in range(3):
@@ -69,11 +69,11 @@ def enlarge(image, factor):
     """
     Enlarge the given image by repeating its pixels along the X and Y axes.
     The parameter 'factor' is the factor by which to enlarge the image. If the
-    provided factor is not within the valid range (1 to 20, for safety reasons),
+    provided factor is not within the valid range (1 to 30, for safety reasons),
     a message is printed, and the original image is returned without modification.
     """
-    if not (1 <= factor <= 20):
-        print("Pick an integer number between 1 and 20.")
+    if not (1 <= factor <= 30):
+        print("Pick an integer number between 1 and 30.")
         return image
 
     else:
@@ -85,6 +85,51 @@ def enlarge(image, factor):
 def pixelize(image, step):
     """
     Pixelize an image by reducing its resolution with a factor 'step'.
+    Adjust the size of the pixelized image to match the original dimensions after enlarging.
     """
-    image_pixelized = image[::step, ::step, :]
+    image_pixelized = image[::step, ::step, :]  # Pixelize the image
+    image_pixelized = enlarge(image_pixelized, step)  # Enlarge the pixelized image
+
+    # Adjust the size of the pixelized image to match the original dimensions
+    image_pixelized = image_pixelized[:image.shape[0], :image.shape[1], :]
+
     return image_pixelized
+
+
+def multiple_manipulations(image, code):
+    """
+    Apply a sequence of image manipulations on the parameter "image", specified
+    by the parameter "code".
+
+    The code is allowed to be a string of characters. Each character in the
+    code corresponds to a specific manipulation:
+    '1' = Horizontal flip, '2' = Vertical flip, '3' = Horizontal and vertical flips,
+    'r' = Color the image red, 'g' = Color the image green, 'b' = Color the image blue,
+    'p': Pixelize the image with a step of 10, 'q': Apply color quantization with a factor of 54.
+
+    Examples:
+        code = [[0, 1, 2, 3], [2, 3, 0, 1], [1, 0, 3, 2], [3, 2, 1, 0]]
+        code = [["0p", 1, "2b", 3], [2, "r3", 0, "q1"], ["g1", 0, "qp3", 2]]
+    """
+    new_image = image.copy()
+    code_str = str(code)
+
+    for letter in code_str:
+        if letter == '1':
+            new_image = h_flip(new_image)
+        if letter == '2':
+            new_image = v_flip(new_image)
+        if letter == '3':
+            new_image = h_flip(v_flip(new_image))
+        if letter == 'r':
+            new_image = color(new_image, 'r')
+        if letter == 'g':
+            new_image = color(new_image, 'g')
+        if letter == 'b':
+            new_image = color(new_image, 'b')
+        if letter == 'p':
+            new_image = pixelize(new_image, 10)
+        if letter == 'q':
+            new_image = color_quantization(new_image, 54)
+
+    return new_image
